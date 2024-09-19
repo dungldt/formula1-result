278cecae228b9485975300ea0c5ead5e7e4dbeae
BBMCode <bbmcode247@gmail.com>
2024-09-19 23:18:52 +0700

    display result using react-boostrap-table2

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,8 +1,42 @@
+import { useEffect, useState } from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit';
+import { getColumnsDataTableFromResultList } from '../util';
 
 const ResultList = ({ list }) => {
+  const { SearchBar } = Search;
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    setColumns(getColumnsDataTableFromResultList(list))
+  }, [columns.length, list]);
+
   return (
-    <div className="col-sm-4">
-    </div>
+    <div>
+      {columns.length > 0 ? (
+        <ToolkitProvider
+          keyField="id"
+          data={list}
+          columns={columns}
+          search
+        >
+          {
+            props => (
+              <div>
+                <div className="my-2">
+                  <SearchBar {...props.searchProps} />
+                </div>
+                <BootstrapTable
+                  classes="react-bootstrap-table"
+                  keyField="id"
+                  {...props.baseProps}
+                />
+              </div>
+            )
+          }
+        </ToolkitProvider>
+      ) : 'No results are currently available'}
+    </div >
   );
 };
 
